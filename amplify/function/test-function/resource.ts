82cf5c3b64d3c2a43010c23f6f27d2c47b20d205
Roshane Pascual <rotp@amazon.com>
2024-01-05 11:17:17 -0800

    add shared secret

--- amplify/function/test-function/resource.ts
+++ amplify/function/test-function/resource.ts
@@ -3,6 +3,7 @@ import { defineFunction, secret } from "@aws-amplify/backend"
 export const testFunction = defineFunction({
     environment: {
         TEST: 'test',
-        AMPLIFY_SECRET: secret('amplifySecret')
+        AMPLIFY_SECRET: secret('amplifySecret'),
+        SHARED_SECRET: secret('sharedSecret'),
     }
 })
